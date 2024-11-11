3b6ba2eb6e1dd7f5c1ab3fd2fd9a329da955dce8
DoangCespedes <84481514+DoangCespedes@users.noreply.github.com>
2024-11-11 13:10:45 -0800

    default port update

--- model/server.js
+++ model/server.js
@@ -5,8 +5,7 @@ class Server {
 
     constructor(){
         this.app = express();
-        this.port = process.env.PORT
-        this.usuariosPath = '/api/usuarios'
+        this.port = process.env.PORT || 3000        this.usuariosPath = '/api/usuarios'
 
         //Middlewares 
         this.middlewares()
